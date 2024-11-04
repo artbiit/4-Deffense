98e3d3536851d9182abffb2903b0cbb5fe02d10d
ljs <artbiit@naver.com>
2024-11-05 08:01:02 +0900

    fix 패킷 파서 스켈레톤 코드 수정
    JWT를 어떻게 관리하는지 찾아봐야함

--- src/constants/handlerIds.js
+++ src/constants/handlerIds.js
@@ -1,7 +1,7 @@
 export const RESPONSE_SUCCESS_CODE = 0;
 
 const GlobalFailCode = Object.freeze({
-  NONE: 0,
+  NONE: RESPONSE_SUCCESS_CODE,
   UNKNOWN_ERROR: 1,
   INVALID_REQUEST: 2,
   AUTHENTICATION_FAILED: 3,
--- src/utils/parser/packetParser.js
+++ src/utils/parser/packetParser.js
@@ -1,48 +1,67 @@
-import { getProtoMessages } from '../../init/loadProtos.js';
-import { getProtoTypeNameByHandlerId } from '../../handlers/index.js';
-import CustomError from '../error/customError.js';
-import { ErrorCodes } from '../error/errorCodes.js';
-import logger from '../logger.js';
+import { getProtoMessages } from "../../init/loadProtos.js";
+import { getProtoTypeNameByHandlerId } from "../../handlers/index.js";
+import CustomError from "../error/customError.js";
+import logger from "../logger.js";
+import configs from "../../configs/configs.js";
+import { getUserBySocket } from "../../session/user.session.js";
 
-export const packetParser = (handlerId, user, data) => {
+const { GlobalFailCode, CLIENT_VERSIONS } = configs;
+
+export const packetParser = (
+  socket,
+  packetType,
+  version,
+  sequence,
+  payloadBuffer
+) => {
+  //버전 호환 필터
+  if (!CLIENT_VERSIONS.includes(version)) {
+    throw new CustomError(GlobalFailCode.INVALID_REQUEST, "VERSION_MISMATCH");
+  }
+
+  const user = getUserBySocket(socket);
+  if (user && user.sequence !== sequence) {
+    throw new CustomError(GlobalFailCode.INVALID_REQUEST, "INVALID_SEQUENCE");
+  }
+
   const protoMessages = getProtoMessages();
-
   // 핸들러 ID에 따라 적절한 payload 구조를 디코딩
-
-  const commonPacket = protoMessages.common.Packet;
-  let packet = null;
+  const gamePacket = protoMessages.GamePacket;
+  let decodedGamePacket = null;
   try {
-    packet = commonPacket.decode(data);
+    decodedGamePacket = gamePacket.decode(payloadBuffer);
   } catch (error) {
     logger.error(error);
     throw new CustomError(
-      ErrorCodes.PACKET_DECODE_ERROR,
-      `패킷 디코딩 중 문제 발생 : common.Packet`,
+      GlobalFailCode.INVALID_REQUEST,
+      `패킷 디코딩 중 문제 발생 : GamePacket`
     );
   }
 
-  const userIdByPacket = packet.userId;
-
   // 핸들러 ID에 따라 적절한 payload 구조를 디코딩
-  const { namespace, typeName } = getProtoTypeNameByHandlerId(handlerId);
+  const { namespace, typeName } = getProtoTypeNameByHandlerId(packetType);
   const PayloadType = protoMessages[namespace][typeName];
   let payload;
   try {
-    payload = PayloadType.decode(packet.payload);
-    payload.userIdByPacket = userIdByPacket;
+    payload = PayloadType.decode(decodedGamePacket.payload);
   } catch (error) {
     logger.error(error);
-    throw new CustomError(ErrorCodes.PACKET_STRUCTURE_MISMATCH, '패킷 구조가 일치하지 않습니다.');
+    throw new CustomError(
+      GlobalFailCode.INVALID_REQUEST,
+      "패킷 구조가 일치하지 않습니다."
+    );
   }
 
   // 필드가 비어 있거나, 필수 필드가 누락된 경우 처리
   const expectedFields = Object.keys(PayloadType.fields);
   const actualFields = Object.keys(payload);
-  const missingFields = expectedFields.filter((field) => !actualFields.includes(field));
+  const missingFields = expectedFields.filter(
+    (field) => !actualFields.includes(field)
+  );
   if (missingFields.length > 0) {
     throw new CustomError(
-      ErrorCodes.MISSING_FIELDS,
-      `필수 필드가 누락되었습니다: ${missingFields.join(', ')}`,
+      GlobalFailCode.INVALID_REQUEST,
+      `필수 필드가 누락되었습니다: ${missingFields.join(", ")}`
     );
   }
 
