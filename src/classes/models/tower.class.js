d457b25dbce945617fd73c149ded49d37b3a3972
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-05 21:05:46 +0900

    add 타워 클래스 추가

---
+++ src/classes/models/tower.class.js
@@ -0,0 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
+
+class Tower {
+  /**
+   * @param {{x: Number, y: Number}} coord 설치할 좌표
+   */
+  constructor(coords) {
+    this.id = uuidv4();
+    
+    this.x = coords.x;
+    this.y = coords.y;
+  }
+}
