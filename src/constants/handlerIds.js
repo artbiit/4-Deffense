beeaf23d211e6cef87aba7807d9988970c7ac7ca
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-05 20:15:47 +0900

    fix handlerIds.js export 형식 통일

--- src/constants/handlerIds.js
+++ src/constants/handlerIds.js
@@ -1,10 +1,8 @@
 export const RESPONSE_SUCCESS_CODE = 0;
 
-const GlobalFailCode = Object.freeze({
+export const GlobalFailCode = Object.freeze({
   NONE: RESPONSE_SUCCESS_CODE,
   UNKNOWN_ERROR: 1,
   INVALID_REQUEST: 2,
   AUTHENTICATION_FAILED: 3,
 });
-
-export { GlobalFailCode };
