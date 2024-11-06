64b3f7e548815c8fa701845d80dcce2c038dd464
ljs <artbiit@naver.com>
2024-11-06 10:39:42 +0900

    fix prettierrc 파일명 오타 수정

--- src/init/index.js
+++ src/init/index.js
@@ -1,18 +1,13 @@
 // 서버 초기화 작업
-import { getProtoMessages, loadProtos } from "./loadProtos.js";
-import { mysql } from "../db/mysql.js";
-import logger from "../utils/logger.js";
-import { connect } from "../db/redis.js";
-import { loadGameAssets } from "./loadAssets.js";
+import { getProtoMessages, loadProtos } from './loadProtos.js';
+import { mysql } from '../db/mysql.js';
+import logger from '../utils/logger.js';
+import { connect } from '../db/redis.js';
+import { loadGameAssets } from './loadAssets.js';
 const initServer = async () => {
   try {
-    await Promise.all([
-      loadGameAssets(),
-      loadProtos(),
-      mysql.init(),
-      connect(),
-    ]);
-    logger.info("All initialized.");
+    await Promise.all([loadGameAssets(), loadProtos(), mysql.init(), connect()]);
+    logger.info('All initialized.');
     // 다음 작업
   } catch (e) {
     logger.error(e);
--- src/init/loadAssets.js
+++ src/init/loadAssets.js
@@ -1,15 +1,15 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import logger from "../utils/logger.js";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import logger from '../utils/logger.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const basePath = path.join(__dirname, "../../assets");
+const basePath = path.join(__dirname, '../../assets');
 
 const readFileAsync = (filename) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(path.join(basePath, filename), "utf8", (err, data) => {
+    fs.readFile(path.join(basePath, filename), 'utf8', (err, data) => {
       if (err) {
         reject(err);
         return;
@@ -23,9 +23,9 @@ let gameAssets = {};
 
 export const loadGameAssets = async () => {
   const [bases, monsters, towers] = await Promise.all([
-    readFileAsync("base.json"),
-    readFileAsync("monster.json"),
-    readFileAsync("tower.json"),
+    readFileAsync('base.json'),
+    readFileAsync('monster.json'),
+    readFileAsync('tower.json'),
   ]);
 
   gameAssets = { bases, monsters, towers };
