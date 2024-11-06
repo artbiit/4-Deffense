2328c186e691153db0641c36f6918df7b62e5e74
ljs <artbiit@naver.com>
2024-11-06 10:52:55 +0900

    fix 매칭 redis 상호작용 코드 전반적인 구상 완료

--- src/db/match/match.redis.js
+++ src/db/match/match.redis.js
@@ -1,26 +1,24 @@
-import { getRedis } from "../redis.js";
-import CustomError from "../../utils/error/customError.js";
-import configs from "../../configs/configs.js";
-import logger from "../../utils/logger.js";
+import { getRedis } from '../redis.js';
+import CustomError from '../../utils/error/customError.js';
+import configs from '../../configs/configs.js';
+import logger from '../../utils/logger.js';
 
 const { GlobalFailCode } = configs;
 
-//*매치메이킹 대기열 키*/
-const WAITING_KEY = "match_queue";
-//** 매치메이킹 점수 정렬 키 */
-const MATCH_SCORE_KEY = "match_scores";
+/**매치메이킹 대기열 키*/
+const WAITING_KEY = 'match_queue';
+/** 매치메이킹 점수 정렬 키 */
+const MATCH_SCORE_KEY = 'match_scores';
 
-//**매치메이킹 등록 함수 */
+/**매치메이킹 등록 함수 */
 export const enqueueMatchMaking = async (userId, bestScore) => {
   try {
     if (!userId) {
-      throw new Error("userId must be defined");
+      throw new Error('userId must be defined');
     }
 
     if (!bestScore || Number.isNaN(bestScore)) {
-      throw new Error(
-        `bestScore must be defined : ${bestScore}[${typeof bestScore}]`
-      );
+      throw new Error(`bestScore must be defined : ${bestScore}[${typeof bestScore}]`);
     }
     const redis = await getRedis();
 
@@ -36,10 +34,51 @@ export const enqueueMatchMaking = async (userId, bestScore) => {
   }
 };
 
+/** 대기열에 등록된 유저 정보 */
 export const dequeueMatchMaking = async () => {
   try {
     const redis = await getRedis();
 
-    const userId = await redis;
-  } catch (error) {}
+    const userId = await redis.lPop(WAITING_KEY);
+    if (!userId) {
+      logger.warn('dequeueMatchMaking. lPop is empty');
+      return null;
+    }
+
+    const userScore = await redis.zScore(MATCH_SCORE_KEY, userId);
+
+    if (!userScore) {
+      logger.warn(`dequeueMatchMaking. could not found Score : ${userId}`);
+      return null;
+    }
+
+    return { userId, userScore };
+  } catch (error) {
+    logger.error(`dequeueMatchMaking. ${error.message}`);
+    throw error;
+  }
+};
+
+/** 대기열 인원 수 반환 */
+export const getQueueCount = async () => {
+  try {
+    const redis = await getRedis();
+    const queueLength = await redis.lLen(WAITING_KEY);
+    return queueLength;
+  } catch (error) {
+    logger.error(`getQueueCount. ${error.message}`);
+    throw error;
+  }
+};
+
+/** 해당 유저 정보 제거 */
+export const removeUserScore = async (userId) => {
+  try {
+    const redis = await getRedis();
+    const removedCount = await redis.zRem(MATCH_SCORE_KEY, userId);
+    return removedCount;
+  } catch (error) {
+    logger.error(`removeUserScore. ${error.message}`);
+    throw error;
+  }
 };
