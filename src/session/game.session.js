56e7589fbd087ace8329a44f6b3ac894c8b700b1
이상민 <yihs97@naver.com>
2024-11-07 14:04:04 +0900

    update 소켓으로 세션찾고 몬스터넣기

--- src/session/game.session.js
+++ src/session/game.session.js
@@ -1,8 +1,9 @@
-import { gameSessions } from "./sessions.js";
-import Game from "../classes/models/game.class.js";
+import { gameSessions } from './sessions.js';
+import Game from '../classes/models/game.class.js';
 
 export const addGameSession = (id) => {
   const session = new Game(id);
+  session.monsters = {}; // 세션에 몬스터 리스트를 추가
   gameSessions.push(session);
   return session;
 };
@@ -17,6 +18,19 @@ export const removeGameSession = (id) => {
 export const getGameSession = (id) => {
   return gameSessions.find((session) => session.id === id);
 };
+
+export const addMonsterToGameSession = (socket, monster) => {
+  const session = getGameSession(socket);
+  if (session) {
+    session.monsters.push(monster);
+    return true; // 성공적으로 추가했음을 반환
+  }
+  return false; // 세션이 없으면 false 반환
+};
+
+export const getGameSessionByUser = (user) => {};
+
+export const getGameSessionByUserId = (userId) => {};
 
 export const getAllGameSessions = () => {
   return gameSessions;
