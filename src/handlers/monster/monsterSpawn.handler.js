648c17bccebedf95a6cbcd2f05cf4d9c5c50744e
이상민 <yihs97@naver.com>
2024-11-07 20:13:19 +0900

    fix monsterSpawn 핸들러 내용 수정

--- src/handlers/monster/monsterSpawn.handler.js
+++ src/handlers/monster/monsterSpawn.handler.js
@@ -1,59 +1,20 @@
-import { RESPONSE_SUCCESS_CODE } from '../../constants/handlerIds.js';
-import { getGameAssets } from '../../init/loadAssets.js'; // getGameAssets 사용해야하나 ?
-import CustomError from '../../utils/error/customError.js';
-import { ErrorCodes } from '../../utils/error/errorCodes.js';
-import { handleError } from '../../utils/error/errorHandler.js';
-import { v4 as uuidv4 } from 'uuid';
-import { createResponse } from '../../utils/response/createResponse.js';
-import {
-  addMonsterToGameSession,
-  getGameSession,
-  getGameSessionByUserId,
-} from '../../session/game.session.js';
+import { addMonsterToGameSession } from '../../session/game.session.js';
 import config from '../../configs/configs.js';
+import Result from '../result.js';
 
 const { PacketType } = config;
 
-export const SpawnMonsterRequestHandler = ({ socket, userId, payload }) => {
-  try {
-    // 소켓으로 유저 찾기
-    const user = getUserBySocket(socket);
-    if (!user) {
-      throw new CustomError(ErrorCodes.USER_NOT_FOUND, '해당 유저를 찾을 수 없습니다.');
-    }
-    // getGameSessionByUser 로 게임 세션 찾기 ?
-    // 아니면 user.id로 게임 세션 찾기?
-    const gameSession = getGameSessionByUserId(user.id);
-    if (!gameSession) {
-      throw new CustomError(ErrorCodes.GAME_NOT_FOUND, '해당 게임 세션을 찾을 수 없습니다.');
-    }
+export const SpawnMonsterRequestHandler = ({ socket, payload }) => {
+  // 1~5 사이의 monsterNumber 생성
+  const monsterNumber = Math.floor(Math.random() * 5) + 1;
 
-    // UUID로 고유한 몬스터 ID 생성
-    const uniqueMonsterId = uuidv4();
+  // 게임 세션에 몬스터 추가
+  const monsterId = addMonsterToGameSession(socket, monsterNumber);
 
-    // 1~5 사이의 monsterNumber 생성
-    const monsterNumber = `MON0000${Math.floor(Math.random() * 5) + 1}`;
-
-    // 세션에 넣을 몬스터 정보
-    const newMonster = {
-      id: uniqueMonsterId,
-      monsterNumber: monsterNumber,
-      level: payload.level || 1, // payload에서 level을 가져오거나 기본레벨 1
-    };
-
-    // 게임 세션에 몬스터 추가
-    addMonsterToGameSession(socket, newMonster);
-
-    // 몬스터 생성 응답
-    const spawnMonsterResponse = createResponse(PacketType.SPAWN_MONSTER_RESPONSE, user, {
-      monsterId: uniqueMonsterId,
-      monsterNumber: monsterNumber,
-      message: '몬스터가 성공적으로 생성되었습니다.',
-    });
-
-    socket.write(spawnMonsterResponse);
-  } catch (error) {
-    handleError(error);
+  if (monsterId) {
+    console.log(`몬스터 추가 됨 : ${monsterId}`);
+    const result = new Result({ monsterId, monsterNumber }, PacketType.SPAWN_MONSTER_RESPONSE);
+    return result;
   }
 };
 /* message S2CSpawnMonsterResponse {
