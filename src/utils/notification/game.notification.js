5fcb90f6f8c2730bcb5df00ea7ce1fdb19932f9c
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-07 11:41:40 +0900

    update S2CAddEnemyTowerNotification 패킷을 작성하는 함수 추가
    - 다른 브랜치에서 완성된 makeNotification 함수에 의존함 (추후 merge때 가져올 예정)

--- src/utils/notification/game.notification.js
+++ src/utils/notification/game.notification.js
@@ -1,6 +1,8 @@
 // import { handlerIds } from '../../handlers/index.js';
 // import { createResponse } from '../response/createResponse.js';
 
+import { PacketType } from '../../constants/header.js';
+
 // export const createLocationPacket = (users) => {
 //   return createResponse(handlerIds['gameNotification.LocationUpdate'], 0, { users }, null);
 // };
@@ -8,3 +10,19 @@
 // export const createPingPacket = (timestamp) => {
 //   return createResponse(handlerIds['common.Ping'], 0, { timestamp }, null);
 // };
+
+export const makeNotification = () => {};
+
+/**
+ * S2CAddEnemyTowerNotification 패킷을 생성하는 함수
+ * @param {User} user 알림을 받을 유저
+ * @param {string} towerId 설치된 적 타워 ID
+ * @param {number} x 설치된 적 타워 x 좌표
+ * @param {number} y 설치된 적 타워 y 좌표
+ * @returns {Buffer} 전송할 패킷
+ */
+export const createAddEnemyTowerNotification = (user, towerId, x, y) => {
+  const packetType = PacketType.ADD_ENEMY_TOWER_NOTIFICATION;
+  const payload = { towerId, x, y };
+  return makeNotification(packetType, user, payload);
+};
