8db280ed3e65b7df6403b5b5c7cd7da995ef7930
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-08 14:25:50 +0900

    update 타워 클래스에 애셋ID와 Power 프로퍼티 추가
    - 타워 생성시 랜덤한 타워를 선택해 해당 타워의 애셋ID와 power를 이용해 초기화하도록 설정

--- src/classes/models/tower.class.js
+++ src/classes/models/tower.class.js
@@ -1,14 +1,20 @@
 import { v4 as uuidv4 } from 'uuid';
+import { getRandomGameAsset } from '../../utils/asset/getAssets.js';
+import { ASSET_TYPE } from '../../constants/assets.js';
 
 class Tower {
   /**
    * @param {{x: Number, y: Number}} coord 설치할 좌표
    */
   constructor(coords) {
+    const towerData = getRandomGameAsset(ASSET_TYPE.TOWER);
+    this.assetId = towerData.id;
     this.instanceId = uuidv4();
 
     this.x = coords.x;
     this.y = coords.y;
+
+    this.power = towerData.Power;
   }
 }
 export default Tower;
