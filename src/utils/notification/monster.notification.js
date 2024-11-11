082cc0db470a29a9c7cee6aa45bab857d93bf455
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-11 11:13:49 +0900

    bugfix 잘못된 변수명 접근

--- src/utils/notification/monster.notification.js
+++ src/utils/notification/monster.notification.js
@@ -9,7 +9,7 @@ import { PacketType } from '../../constants/header.js';
  * @returns {Buffer} 전송할 패킷
  */
 export const createEnemyMonsterDeathNotification = (user, monster) => {
-  const packetType = configs.packetType.ENEMY_MONSTER_DEATH_NOTIFICATION;
+  const packetType = configs.PacketType.ENEMY_MONSTER_DEATH_NOTIFICATION;
   const payload = { monsterId: monster.id };
   return makeNotification(packetType, payload, user);
 };
