ddc16e9c407a2a638699226f872442d886e6dd9c
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-11 20:05:50 +0900

    fix 몬스터 소환 주기 클라이언트에 수치에 맞춰서 수정 1초 -> 2초

--- src/constants/game.js
+++ src/constants/game.js
@@ -2,5 +2,5 @@ export const MONSTER_KILL_SCORE = 100;
 export const MONSTER_KILL_GOLD = 100;
 
 export const INITIAL_GOLD = 10000;
-export const MONSTER_SPAWN_INTERVAL = 1000; // 몬스터 소환 주기
+export const MONSTER_SPAWN_INTERVAL = 2000; // 몬스터 소환 주기
 export const SYNC_INTERVAL = 1000; // 동기화 주기
